4cc3af6c00093a213e3eccb6b43e5ec3392c326a
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-05-14 17:04:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,26 @@
 class Application
   
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello, World"
+    resp.finish
+    
+    t = Time.now
+    
+    if t.hour < 12
+      puts "Good Morning!"
+    else 
+      puts "Good Afternoon!"
+    end 
+      
+    
+    
+    
+    
+  end
+  
+  
+  
   t = Time.now
   
   if t.hour < 12
